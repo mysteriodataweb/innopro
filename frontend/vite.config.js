d4c82e458d0a3b33ccb5c4f1a3ec4d58334d0e0f
Solangeilinga <solangeilinga@gmail.com>
2026-06-14 11:30:51 +0000

    ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target:'https://crusher-ambition-drone.ngrok-free.dev',
         changeOrigin: true,
         rewrite: (p) => p.replace(/^\/api/, '/api/v1'),
       },
